f85035baabeedb44547d8b1f83e78985c5b8cd9d
KwarK <KwarK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-10 14:48:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kw-essential-share-buttons/tags/1.0@395552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kw-primo-share-button.php
+++ kw-primo-share-button.php
@@ -13,7 +13,7 @@
  Description: An ultra light way to display facebook share button, google buzz button, twitter tweet this button, msn live share button, windows live share button.
  Author: Laurent (KwarK) Bertrand
  Version: 1.0
- Tags: share-button, primordial-share-button, ultra-light-plugin-share, tweeter-share, tweeter-tweet-this, facebook-share, facebook-like, google-buzz, google-share, msn-live-share, windows-live-share, share-button-auto-integration
+ Tags: share-button, primordial-share-button, ultra-light-plugin-share, tweeter-share, tweeter-tweet-this, facebook-share, facebook-like, google-buzz, google-share, msn-windows-live-share, share-button-auto-integration
  
  Author URI: Laurent Bertrand | KwarK on http://www.style-cataclysm.com/
  */
