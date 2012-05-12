0812dc88bc0bbd0f1527770614c0fa6e5acb75b3
KwarK <KwarK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-12 15:46:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kw-essential-share-buttons/tags/1.1@543323 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,45 @@
+=== Kw Essential share buttons ===
+Contributors: Laurent (KwarK) Bertrand
+Donate link: http://kwark.allwebtuts.net/
+Tags: button, sharing, tweeter-tweet, facebook-like, g-plus-one, msn-windows-live-sharing, wordpress, plugin
+Requires at least: 3.0
+Tested up to: 3.3.2
+Stable tag: 1.1
+
+A simple plugin to display facebook like button, google +1 button, twitter button, msn live share button.
+
+== Description ==
+
+* A simple plugin to display facebook like button, google +1 button, twitter button, msn live share button only on posts, without administration options.
+* No settings to change, this plugin added automatically multiple sharing buttons in your posts.
+
+
+== Installation ==
+
+1. Upload 'kw-essential-share-buttons' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. No screen
+
+== Frequently Asked Questions ==
+
+View forum support on Wordpress for more information
+
+
+== Upgrade Notice ==
+
+1. Use the Wordpress automatic upgrade notice or upgrade this plugin manually
+
+
+== Changelog ==
+
+= 1.0.1 =
+
+* Fixed image for Windows live share
+
+= 1.0 = 
+
+* Original review
+
---
+++ kw-primo-share-button.php
@@ -0,0 +1,63 @@
+<?php 
+/*
+Plugin Name: Kw Essential share buttons
+Plugin URI: http://kwark.allwebtuts.net
+Description: An ultra light way to display facebook like button, google +1 button, twitter button, msn live share button. No configuration needed.
+Author: Laurent (KwarK) Bertrand
+Version: 1.1
+Tags: sharing-button, primordial-sharing-button, ultra-light-plugin-share, tweeter-share, tweeter-tweet-this, facebook-share, facebook-like, google-buzz, google-share, msn-live-share, windows-live-share 
+Author URI: http://kwark.allwebtuts.net
+ */
+/*  
+	Copyright 2012  Laurent (KwarK) Bertrand  (email : kwark@allwebtuts.net)
+	
+	You can not remove this comments such as my informations.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+$plugin_url = WP_PLUGIN_URL . '/kw-essential-share-buttons/';
+/**Add general css to frontend **/
+wp_register_style( 'kw_button_share', '' .$plugin_url. 'css/general.css');
+
+/**Main function **/
+function kw_share_buttons($content)
+{
+	if(is_single())
+	{
+		wp_enqueue_style( 'kw_button_share');
+		
+		echo '<div class="kw-share-this">
+		
+			<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a>
+            <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+				
+			<div class="gplusone">
+				<script src="http://apis.google.com/js/plusone.js" type="text/javascript"></script>
+				<g:plusone size="medium"></g:plusone>
+			</div>
+			
+			<a class="msn-share-button" href="#" onclick="javascript:window.open(\'http://profile.live.com/badge?url=' . get_permalink($post->post_parent) .'/\', \'Windows Live\', \'width=550, height=450, top=230, right=450, left=450\');" title="Messenger live share">Partager</a>
+			
+			<div class="facebook-share-button"><iframe src="http://www.facebook.com/plugins/like.php?href=' . get_permalink($post->post_parent)
+    .'&layout=button_count&show_faces=false&width=85&action=like&colorscheme=light&height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:21px;" allowTransparency="true"></iframe>
+            </div>
+      </div>';		
+		
+		return $content;
+	}
+}
+/** Main function to posts **/
+add_filter('the_content', 'kw_share_buttons');
+?>
