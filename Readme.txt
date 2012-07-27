71afe9f3a4ddf997ffa8f998af3207b9f8b50e4c
KwarK <KwarK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-27 12:14:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kw-essential-share-buttons/tags/1.2@578056 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,59 @@
+=== Kw Essential share buttons ===
+
+Contributors: KwarK
+Donate link: http://kwark.allwebtuts.net/
+Tags: essential, sharing, social, media, share, button, buttons, pinterest, facebook, twitter, gplus, linkedin, ultra, light, plugin, wordpress
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 1.2
+
+mini buttons for sharing posts by users on social media. Buttons and popup for pinterest, linkedin, facebook, google +1, twitter, windows live
+
+== Description ==
+
+An ultra light way to display mini buttons for sharing post directly on social media. Buttons and popup for pinterest, linkedin, facebook, google +1, twitter, windows live. Buttons are visible on each post in home page loop and in if is single view. No configuration needed.
+
+* It is not filled options. Zero option, only essential buttons and only 3ko.
+* A simple plugin to display facebook like button, google +1 button, twitter button, msn live share button only on posts, without administration options.
+* No settings to change, this plugin added automatically multiple sharing buttons in your posts.
+
+
+== Installation ==
+
+1. Upload 'kw-essential-share-buttons' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. No screen
+
+== Frequently Asked Questions ==
+
+View forum support on Wordpress for more information
+
+
+== Upgrade Notice ==
+
+1. Use the Wordpress automatic upgrade notice or upgrade this plugin manually
+
+
+== Changelog ==
+
+= 1.2 =
+
+* Now displaying also for posts on home page
+* Code review with mini buttons
+* Added pinterest, linkedin
+
+= 1.1 =
+
+* Code review.
+
+= 1.0.1 =
+
+* Fixed image for Windows live share
+
+= 1.0 = 
+
+* Original review
+
---
+++ kw-primo-share-button.php
@@ -0,0 +1,75 @@
+<?php 
+/*
+Plugin Name: Kw Essential share buttons
+Plugin URI: http://kwark.allwebtuts.net
+Description: An ultra light way to display mini buttons for sharing on social media. Buttons and popup for pinterest, linkedin, facebook, google +1, twitter, windows live on posts in home page loop and in if is single. No configuration needed.
+Author: Laurent (KwarK) Bertrand
+Version: 1.2
+Tags:  essential, sharing, social, media, share, button, pinterest, facebook, twitter, gplus, linkedin, ultra, light, plugin, wordpress
+Author URI: http://kwark.allwebtuts.net
+ */
+/*  
+	Copyright 2012  Laurent (KwarK) Bertrand  (email : kwark@allwebtuts.net)
+	
+	You can not remove this comments such as my informations.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+global $plugin_url;
+
+$plugin_url = WP_PLUGIN_URL . '/kw-essential-share-buttons/';
+
+wp_register_style( 'kw_button_share', '' .$plugin_url. 'css/general.css');
+
+function kw_share_buttons($content)
+{
+	if(is_single() || is_home())
+	{
+		global $plugin_url, $post;
+		
+		wp_enqueue_style( 'kw_button_share');
+		?>
+        <div class="kw-share-this">
+                
+                <a href="#" onclick="javascript:window.open('http://profile.live.com/badge?url=<?php urlencode(the_permalink()); ?>', 'Windows Live', 'width=550, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+                <img src="<?php echo esc_url($plugin_url . 'images/blt8.png', array('http', 'https')); ?>" alt="messenger live share" title="Live" /></a>
+                
+                <a href="#" onclick="javascript:window.open('https://plusone.google.com/_/+1/confirm?url=<?php urlencode(the_permalink()); ?>', 'Google plusone', 'width=550, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+                <img src="<?php echo esc_url($plugin_url . 'images/google.png', array('http', 'https')); ?>" alt="Google plusone" title="Plus" /></a>
+                
+                <a href="#" onclick="javascript:window.open('http://twitter.com/share?url=<?php urlencode(the_permalink()); ?>&text=<?php urlencode(the_title()); ?>', 'Twitter', 'width=550, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+                <img src="<?php echo esc_url($plugin_url . 'images/twitter.png', array('http', 'https')); ?>" alt="Twitter" title="Tweet" /></a>
+                
+                 <a href="#" onclick="javascript:window.open('https://www.facebook.com/login.php?skip_api_login=1&display=popup&nux=1&referer=<?php echo urlencode(get_site_url()); ?>&social_plugin=like&external_page_url=<?php urlencode(the_permalink()); ?>', 'Facebook', 'width=550, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+                <img src="<?php echo esc_url($plugin_url . 'images/facebook.png', array('http', 'https')); ?>" alt="Facebook" title="Like" /></a>
+                
+                <a href="#" onclick="javascript:window.open('http://www.linkedin.com/shareArticle?mini=true&url=<?php urlencode(the_permalink()); ?>&title=<?php urlencode(the_title()); ?>&source=<?php echo urlencode(get_site_url()); ?>', 'LinkedIn', 'width=550, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+                <img src="<?php echo esc_url($plugin_url . 'images/linkedin.png', array('http', 'https')); ?>" alt="LinkedIn" title="In" /></a>
+                
+				<?php if (has_post_thumbnail())
+				{ 
+					$kw_thumb_id = get_post_thumbnail_id($post->ID);
+				}
+				?>
+                
+                <a href="#" onclick="javascript:window.open('http://pinterest.com/pin/create/button/?url=<?php urlencode(the_permalink()); ?>&media=<?php echo urlencode(wp_get_attachment_url($kw_thumb_id)); ?>&description=<?php urlencode(the_title()); ?>', 'Pinterest', 'width=560, height=450, top=230, right=450, left=450'); return false;" rel="nofollow">
+            <img src="http://passets-ec.pinterest.com/images/about/buttons/small-p-button.png" alt="Pinterest" title="Pin" /></a>    
+        </div>
+        <?php
+	}
+	return $content;
+}
+add_filter('the_content', 'kw_share_buttons');
+?>
